839199023b3bfc553285f516648aaee0e8bc5112
SamarthMabrukar <sam.mabrukar1@gmail.com>
2025-04-19 21:45:51 +0530

    Till Surface Normals

---
+++ Chapter_6_Surface_Normals/color.h
@@ -0,0 +1,24 @@
+#ifndef __HCOLOR__
+#define __HCOLOR__
+
+#include <iostream>
+#include "vec3.h"
+
+using color = vec3;
+
+void write_color(std::ostream &out, const color &pixel_color)
+{
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Translate [0,1] component values to the byte range [0,255].
+    int rbyte = int(255.99 * r);
+    int gbyte = int(255.99 * g);
+    int bbyte = int(255.99 * b);
+
+    // Write the pixel color components.
+    out << rbyte << " " << gbyte << " " << bbyte << "\n";
+}
+
+#endif //__HCOLOR__
